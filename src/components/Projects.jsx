5b364496bb644ea96e43a0c985dc77d94f8f6ba0
Maria Lima <mariaclimany@gmail.com>
2026-01-09 13:36:44 -0500

    add text to card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,8 +20,8 @@ const projects = [
   {
     title: "AI Task System",
     description:
-      "Intelligent task management powered by AI to optimize productivity from LangChain Academy learning course.",
-    tech: ["OpenAI","Python", "Docker"],
+      "Intelligent task management powered by AI with long-term memory to optimize productivity.  Code from LangChain Academy learning course.",
+    tech: ["OpenAI","LangGraph", "LangChain", "Python", "Docker", "Trustcall", "Pydantic"],
     githubLink: 'https://github.com/marialima/generative-ai/tree/main/task-maestro',
     liveLink: 'https://tasks.mariaclima.ai/docs',
   }
